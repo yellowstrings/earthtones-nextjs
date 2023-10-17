df0545482ce6093d7542e03669a9a9c208c460c5
Anisah Majeed <anisahmajeed4@gmail.com>
2023-10-17 17:55:05 -0400

    added about section and deleted commented out code

---
+++ pages/components/about.js
@@ -0,0 +1,24 @@
+"use client"
+
+import React from "react";
+import { Button } from '@mui/material';
+
+
+import "../../src/app/globals.css"
+
+const About = () => {
+
+  //TODO: edit bio
+  return (
+    <div className="flex-2 ">
+      <text className=" marginHorizontal aboutText" >
+        about
+      </text>
+      <text className=" marginHorizontal aboutText">
+      earth tones' compositions are a reflection of their personal experiences and turbulent emotions. haunting strings, raw vocals, and intimacy are the trademark sounds of an earth tones record.
+      </text>
+    </div>
+  )
+}
+
+export default About;
