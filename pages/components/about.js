a8a8796a322a178621ddeace122708add4e2329d
anisah majeed <anisahmajeed4@gmail.com>
2024-08-19 12:09:08 -0400

    show text and styling changes

--- pages/components/about.js
+++ pages/components/about.js
@@ -1,7 +1,6 @@
 "use client"
 
 import React from "react";
-import { Button } from '@mui/material';
 
 
 import "../../src/app/globals.css"
@@ -17,10 +16,10 @@ const About = () => {
           </text>
         </div>
         <div className="margin-horizontal body-text">
-          <p className="spacer">
+          <p className="spacer readable-background-light">
           To be a musician is almost an intimate act, to cut oneself open and shill the contents. Then again, perhaps that's what drew earth tones to it in the first place. Always having had trouble finding themself, it proved a release from the self-imposed rigidity of morality, conscience and the all-consuming fear of being a burden. Putting oneself first, a recurring theme of their 2024 debut EP pained smiles, has never come easily to earth tones— yet, after amassing a humble following from performing in a series of coffee shops and bars in their hometown of Orlando, FL, they took a leap of faith in moving to the flowering, vibrant music scene of Austin, Texas. However, as the journey unfolds, and as they gradually come to terms with the enormous (1140 miles, to be exact) move, it has only led to a massive efflorescence of the soul— and, ultimately, their music.           
           </p>
-          <p>
+          <p className="readable-background-dark">
           Conceivably, the creator of pained smiles, earth tones' 2024 EP, has something of a melancholic relationship to music— as any classical violinist that gets their start in their teens tends to have, from not-enough to too-much. Perhaps it's therapeutic, or maybe it's just the glorified act of digging your nails into a still-bleeding wound. To listen to pained smiles is to peer within their naked heart, from the painfully vulnerable and intimate lyricism to the sultry, delicate exploration of self. Now having released pained smiles, earth tones finds themself shackled and liberated, simultaneously, by their quiet, controlled fierceness, fervent dedication to music, and— most of all— their Capricorn stellium.
           </p>
         </div>
--- pages/components/footer.js
+++ pages/components/footer.js
@@ -1,16 +1,12 @@
 "use client"
 
-import React, { useEffect, useState } from "react";
-import { Button } from '@mui/material';
-import { FaInstagram, FaTiktok, FaGithub } from "react-icons/fa";
+import React from "react";
 
  import InstagramButton from "./social/InstagramButton.js";
  import TikTokButton from "./social/TikTokButton.js";
  import GithubButton from "./social/GithubButton.js";
 
 import "../../src/app/globals.css"
-
-// import { colorTheme } from '../utils/theme'
 
 const Footer = () => {
 
